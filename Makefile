70924cc8f625f1ab7ffc875e06c06ef5724d6939
“Xuqiuye <xuqiuye.li@gmail.com>
2024-02-02 11:27:35 +0900

    initial files

---
+++ Makefile
@@ -0,0 +1,6 @@
+start:
+	./gradlew bootrun
+test:
+	./gradlew clean test
+lint:
+	./gradlew --continue ktlintFormat
---
+++ src/main/kotlin/com/xuqiuye/server/ServerApplication.kt
@@ -0,0 +1,11 @@
+package com.xuqiuye.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerApplication>(*args)
+}
